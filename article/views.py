cd4e953d1fcb19b88ff559360caac7d675823ea2
deidrebrin <dwhitmore@g.ucla.edu>
2021-02-02 11:30:38 -0800

    Add 404 exception and build out main article page a bit further

--- article/views.py
+++ article/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 # from django.template import loader
 
 from .models import Article
@@ -16,4 +16,12 @@ def article_list(request):
   return render(request, 'article/article_list.html', context)
 
 def article(request, article_id):
-  return HttpResponse('%s' % article_id)
+  try:
+    article = Article.objects.get(pk=article_id)
+  except Article.DoesNotExist:
+    raise Http404("Article does not exist.")
+  context = {
+    'article': article
+  }
+  return render(request, 'article/article.html', context)
+# can be written as a shortcut (get_object_or_404())
