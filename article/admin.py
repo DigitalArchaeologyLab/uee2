fccae34bfe300202fce79f8829ca8eb056b2427f
deidrebrin <dwhitmore@g.ucla.edu>
2021-02-16 16:35:09 -0800

    Implement fieldsets in Article admin interface

--- article/admin.py
+++ article/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
-from markdownx.admin import MarkdownxModelAdmin
+# from markdownx.admin import MarkdownxModelAdmin
 
 # Register your models here.
 from .models import Author
 admin.site.register(Author)
+
 from .models import Article
-admin.site.register(Article, MarkdownxModelAdmin)
+class ArticleAdmin(admin.ModelAdmin):
+  fieldsets = [
+    ('Title', {'fields': ['title_eng', 'title_ar', 'title_fr', 'title_de']}),
+    ('Author(s)', {'fields': ['author_id']}),
+    ('Abstract', {'classes': ('collapse',),'fields': ['abstract_eng', 'abstract_ar']}),
+    ('Keywords', {'fields': ['keywords']}),
+    ('Article', {'fields': ['body']}),
+  ]
+admin.site.register(Article, ArticleAdmin)
+
 from .models import Keyword
 admin.site.register(Keyword)
