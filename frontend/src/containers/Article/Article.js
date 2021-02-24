35a5c147d380f460b6a97f0392c29e2f5a5a6094
deidrebrin <dwhitmore@g.ucla.edu>
2021-02-24 15:46:49 -0800

    Set up draft of a full article display
    -Stub out a connection to the API to grab individual article data

--- frontend/src/containers/Article/Article.js
+++ frontend/src/containers/Article/Article.js
@@ -1,9 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ArticleBody from "../../components/ArticleBody/ArticleBody";
+import Frontmatter from "../../components/Frontmatter/Frontmatter";
 
-class Article extends React.Component {
-  render() {
-    return <h1>Article</h1>
-  }
+
+function Article(props) {
+  const [article, setArticle] = useState([
+    {
+      id: 0,
+      title_eng: "",
+      title_ar: "",
+      author_id: "",
+      abstract_eng: "",
+      abstract_ar: "",
+      body: "",
+    },
+  ]);
+
+  const refreshList = () => {
+    axios
+      .get("/api/articles/1")
+      .then((res) => setArticle(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    refreshList();
+  }, []);
+
+  return (
+    <div>
+      <Frontmatter title_eng={article.title_eng} title_ar={article.title_ar} author={article.author_id} abstract_eng={article.abstract_eng} abstract_ar={article.abstract_ar} />
+      <ArticleBody body={article.body} />
+    </div>
+  );
 }
 
 export default Article;
