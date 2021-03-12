e26984a135f005c7ba1f4cfdd97fd9b2f1095d6a
deidrebrin <dwhitmore@g.ucla.edu>
2021-03-12 15:49:28 -0800

    Add keywords to REST API

--- backend/article/serializers.py
+++ backend/article/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Article
+from .models import Article, Keyword
 
 class ArticleSerializer(serializers.ModelSerializer):
     authors = serializers.StringRelatedField(many=True)
@@ -7,3 +7,8 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ['id', 'title_eng', 'title_ar', 'title_de', 'title_fr', 'authors', 'abstract_eng', 'abstract_ar', 'keywords', 'status', 'body']
+
+class KeywordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Keyword
+        fields = ['id', 'name_eng', 'name_ar']
