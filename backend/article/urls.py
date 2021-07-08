2ed0722d4114b0d1b5feefca4406befa8d15ad32
deidrebrin <dwhitmore@g.ucla.edu>
2021-07-07 18:41:12 -0700

    Add spatial and temporal models to start timemap

--- backend/article/urls.py
+++ backend/article/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index'),
-  path('all/', views.article_list, name='article-list'),
-  path('article/<int:article_id>/', views.article, name='article'),
+    path("", views.index, name="index"),
+    path("all/", views.article_list, name="article-list"),
+    path("article/<int:article_id>/", views.article, name="article"),
 ]
--- backend/article/views.py
+++ backend/article/views.py
@@ -6,30 +6,32 @@ from .models import Article, Keyword
 
 #### Basic Django view setup to be used for testing ####
 def index(request):
-  return HttpResponse("Hello, this will be used to render an article")
+    return HttpResponse("Welcome to the UEE")
+
 
 def article_list(request):
-  article_titles = Article.objects.all()
-  context = {
-    'article_titles': article_titles,
-  }
-  return render(request, 'article/article_list.html', context)
+    article_titles = Article.objects.all()
+    context = {
+        "article_titles": article_titles,
+    }
+    return render(request, "article/article_list.html", context)
+
 
 def article(request, article_id):
-  try:
-    article = Article.objects.get(pk=article_id)
-  except Article.DoesNotExist:
-    raise Http404("Article does not exist.")
-  context = {
-    'article': article
-  }
-  return render(request, 'article/article.html', context)
+    try:
+        article = Article.objects.get(pk=article_id)
+    except Article.DoesNotExist:
+        raise Http404("Article does not exist.")
+    context = {"article": article}
+    return render(request, "article/article.html", context)
+
 
 #### REST API setup ####
 class ArticleView(viewsets.ModelViewSet):
-  serializer_class = ArticleSerializer
-  queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+
 
 class KeywordView(viewsets.ModelViewSet):
-  serializer_class = KeywordSerializer
-  queryset = Keyword.objects.all()
+    serializer_class = KeywordSerializer
+    queryset = Keyword.objects.all()
