f0d368583dc6b9c9c0627f5c2e692e32ec8d5e23
deidrebrin <dwhitmore@g.ucla.edu>
2021-07-13 08:14:54 -0700

    Remove migration from entrypoint
    - Consider migrating entrypoint to docker-compose file

--- docker/backend/wsgi-entrypoint.sh
+++ docker/backend/wsgi-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-until pipenv run python manage.py migrate; do
-    echo "Waiting for db to be ready..."
-    sleep 2
-done
+# until pipenv run python manage.py migrate; do
+#     echo "Waiting for db to be ready..."
+#     sleep 2
+# done
 
 pipenv run gunicorn uee.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
