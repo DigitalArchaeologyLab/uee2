41e3cec0966df79d419fb0da8a53f0d2f78793e7
deidrebrin <dwhitmore@g.ucla.edu>
2021-07-16 09:48:47 -0700

    Build out tree parser component and adapt SubjectTree to use

--- frontend/src/components/SubjectTree/SubjectTree.js
+++ frontend/src/components/SubjectTree/SubjectTree.js
@@ -3,11 +3,11 @@ import axios from "axios";
 
 import { makeStyles } from "@material-ui/core/styles";
 import TreeView from "@material-ui/lab/TreeView";
+import TreeItem from "@material-ui/lab/TreeItem";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 
-import Subject from "../Subject/Subject";
-import GetSubjectTree from "./GetSubjectTree";
+import ParseTree from "../../elements/Tree/ParseTree";
 
 const useStyles = makeStyles({
   root: {
@@ -41,19 +41,11 @@ function SubjectTree(props) {
     getSubjects();
   }, []);
 
-  // return (
-  //   <div className="subjectTree">
-  //     <TreeView
-  //       className={classes.root}
-  //       defaultCollapseIcon={<ExpandMoreIcon />}
-  //       defaultExpandIcon={<ChevronRightIcon />}
-  //     >
-  //       {SubjectTree.map((subject) => (
-  //         <Subject key={subject.id} subject_id={subject.id.toString()} name={subject.name} />
-  //       ))}
-  //     </TreeView>
-  //   </div>
-  // );
+  const renderTree = (nodes) => (
+    <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
+      {Array.isArray(nodes.children) ? nodes.children.map((node) => renderTree(node)) : null}
+    </TreeItem>
+  );
 
     return (
     <TreeView
@@ -62,70 +54,9 @@ function SubjectTree(props) {
       defaultExpanded={['root']}
       defaultExpandIcon={<ChevronRightIcon />}
     >
-      {/* {renderTree(data)} */}
-      <GetSubjectTree />
+      <ParseTree treeData={renderTree(SubjectTree)} />
     </TreeView>
   );
 }
 
 export default SubjectTree;
-
-
-// example to try!
-// import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import TreeView from '@material-ui/lab/TreeView';
-// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-// import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-// import TreeItem from '@material-ui/lab/TreeItem';
-
-// const data = {
-//   id: 'root',
-//   name: 'Parent',
-//   children: [
-//     {
-//       id: '1',
-//       name: 'Child - 1',
-//     },
-//     {
-//       id: '3',
-//       name: 'Child - 3',
-//       children: [
-//         {
-//           id: '4',
-//           name: 'Child - 4',
-//         },
-//       ],
-//     },
-//   ],
-// };
-
-// const useStyles = makeStyles({
-//   root: {
-//     height: 110,
-//     flexGrow: 1,
-//     maxWidth: 400,
-//   },
-// });
-
-// export default function RecursiveTreeView() {
-//   const classes = useStyles();
-
-//   const renderTree = (nodes) => (
-//     <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
-//       {Array.isArray(nodes.children) ? nodes.children.map((node) => renderTree(node)) : null}
-//     </TreeItem>
-//   );
-
-//   return (
-//     <TreeView
-//       className={classes.root}
-//       defaultCollapseIcon={<ExpandMoreIcon />}
-//       defaultExpanded={['root']}
-//       defaultExpandIcon={<ChevronRightIcon />}
-//     >
-//       {renderTree(data)}
-//     </TreeView>
-//   );
-// }
-
