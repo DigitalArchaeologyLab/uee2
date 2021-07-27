2d62517097211ce6db1c0952984ba5cc27f172fd
deidrebrin <dwhitmore@g.ucla.edu>
2021-07-27 09:18:50 -0700

    Remove extraneous urls
    - React router control preferred

--- backend/article/urls.py
+++ backend/article/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("all/", views.article_list, name="article-list"),
-    path("article/<int:article_id>/", views.article, name="article"),
-    path("subjects/", views.subjects, name="subjects"),
 ]
