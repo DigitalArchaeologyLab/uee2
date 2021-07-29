528c72a37096bc2f18ba6ba3b7384216b126509c
deidrebrin <dwhitmore@g.ucla.edu>
2021-07-29 15:21:50 -0700

    Add Footer to existing containers

--- frontend/src/containers/Article/Article.js
+++ frontend/src/containers/Article/Article.js
@@ -5,25 +5,24 @@ import "./Article.css";
 import ArticleBody from "../../components/ArticleBody/ArticleBody";
 import Frontmatter from "../../components/Frontmatter/Frontmatter";
 import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 function Article(props) {
-  const [article, setArticle] = useState(
-    {
-      id: 0,
-      title_eng: "",
-      title_ar: "",
-      title_de: "",
-      title_fr: "",
-      authors: [""],
-      abstract_eng: "",
-      abstract_ar: "",
-      keywords: [""],
-      body: "",
-      status: "",
-    },
-  );
+  const [article, setArticle] = useState({
+    id: 0,
+    title_eng: "",
+    title_ar: "",
+    title_de: "",
+    title_fr: "",
+    authors: [""],
+    abstract_eng: "",
+    abstract_ar: "",
+    keywords: [""],
+    body: "",
+    status: "",
+  });
 
-  let {id} = useParams();
+  let { id } = useParams();
 
   const getArticle = () => {
     axios
@@ -39,21 +38,22 @@ function Article(props) {
   return (
     <div>
       <Header />
-      <div className='article'>
-      <Frontmatter
-        article_id={article.id}
-        title_eng={article.title_eng}
-        title_ar={article.title_ar}
-        title_de={article.title_de}
-        title_fr={article.title_fr}
-        authors={article.authors}
-        abstract_eng={article.abstract_eng}
-        abstract_ar={article.abstract_ar}
-        keywords={article.keywords}
-      />
-      <hr></hr>
-      <ArticleBody body={article.body} />
+      <div className="article">
+        <Frontmatter
+          article_id={article.id}
+          title_eng={article.title_eng}
+          title_ar={article.title_ar}
+          title_de={article.title_de}
+          title_fr={article.title_fr}
+          authors={article.authors}
+          abstract_eng={article.abstract_eng}
+          abstract_ar={article.abstract_ar}
+          keywords={article.keywords}
+        />
+        <hr></hr>
+        <ArticleBody body={article.body} />
       </div>
+      <Footer />
     </div>
   );
 }
--- frontend/src/containers/Subjects/SubjectBrowse.js
+++ frontend/src/containers/Subjects/SubjectBrowse.js
@@ -3,6 +3,7 @@ import "./SubjectBrowse.css";
 import ArticlesBySubject from "../../components/SubjectTree/ArticlesBySubject";
 import SubjectFacet from "../../components/SubjectTree/SubjectFacet";
 import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 function SubjectBrowse(props) {
   const [selectedSubject, setSelectedSubject] = useState([]);
@@ -17,6 +18,7 @@ function SubjectBrowse(props) {
         />
         <ArticlesBySubject selectedSubject={selectedSubject} />
       </div>
+      <Footer />
     </div>
   );
 }
