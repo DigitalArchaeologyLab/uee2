1810f2efac54a6ed6788ae58b01775f6827ed11c
Deidre Brin <dwhitmore@g.ucla.edu>
2021-09-20 15:21:52 -0700

    Add simple formatting to the glossary

--- frontend/src/containers/Glossary/Glossary.js
+++ frontend/src/containers/Glossary/Glossary.js
@@ -11,9 +11,27 @@ function Glossary() {
     <div>
       <Header />
       <div className="glossary">
+        <div className="glossary__title">
+          <h1>Glossary</h1>
+        </div>
+        <div className="glossary__desc">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tempor
+            varius turpis neque, fermentum lectus adipiscing. In nec commodo
+            lectus enim morbi. Sit posuere dolor sed facilisis eget. Egestas non
+            ipsum condimentum lectus tortor. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Tempor varius turpis neque, fermentum
+            lectus adipiscing. In nec commodo lectus enim morbi. Sit posuere
+            dolor sed facilisis eget. Egestas non ipsum condimentum lectus
+            tortor.
+          </p>
+        </div>
         {/* <PlaceList /> */}
         {/* <PeriodList /> */}
-        <TermList />
+        <div className="terms">
+          <h2>Terms</h2>
+          <TermList />
+        </div>
       </div>
       <Footer />
     </div>
