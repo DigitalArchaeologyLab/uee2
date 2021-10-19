1b34bf48b1c8234910ff549a8f3fb908da875628
Deidre Brin <dwhitmore@g.ucla.edu>
2021-10-19 14:28:56 -0700

    Update editor widget to use Place instead of Location

--- backend/article/static/article/insertLocation.js
+++ backend/article/static/article/insertLocation.js
@@ -1,6 +1,6 @@
-function tagLocation() {
+function tagPlace() {
   // Get the modal
-  var modal = document.getElementById("tagLocationModal");
+  var modal = document.getElementById("tagPlaceModal");
 
   // Open the modal
   modal.style.display = "block";
@@ -21,19 +21,19 @@ function tagLocation() {
   };
 }
 
-async function getLocations() {
-  const response = await axios.get("/api/locations/");
-  const locationsObj = await response.data;
-  let locations = [];
-  await locationsObj.map((location) => {
-    locations.push(`${location.name_eng}`);
+async function getPlacess() {
+  const response = await axios.get("/api/places/");
+  const placesObj = await response.data;
+  let places = [];
+  await placesObj.map((place) => {
+    places.push(`${place.name_eng}`);
   });
-  return locations;
+  return places;
 }
 
-function prepLocationSelectOptions() {
-  getLocations().then((elements) => {
-    let select = document.getElementById("id_locations_modal");
+function prepPlaceSelectOptions() {
+  getPlaces().then((elements) => {
+    let select = document.getElementById("id_places_modal");
     for (let el of elements) {
       let option = document.createElement("option");
       option.text = el;
@@ -43,33 +43,33 @@ function prepLocationSelectOptions() {
   });
 }
 
-function insertLocationSelections() {
+function insertPlaceSelections() {
   // get the text from the article body that has been highlighted
   let selectedText = id_body.value.slice(
     id_body.selectionStart,
     id_body.selectionEnd
   );
 
-  // get full list of locations from selection window
-  const locationOptions = document.getElementById("id_locations_modal");
+  // get full list of places from selection window
+  const placeOptions = document.getElementById("id_places_modal");
   event.preventDefault();
-  // get which location was selected
-  const selectedLocation = locationOptions.selectedIndex;
-  // embed tag with appropriate location id
+  // get which place was selected
+  const selectedPlace = placeOptions.selectedIndex;
+  // embed tag with appropriate place id
   id_body.setRangeText(
-    `<span class="taggedLocation" id="${selectedLocation}">${selectedText}</span>`
+    `<span class="taggedPlace" id="${selectedPlace}">${selectedText}</span>`
   );
 
-  // add the tagged location to the locations field so that we can connect the article to these locations on the timemap
-  const locationField = document.getElementById("id_location");
-  const locationFieldOptions = locationField.options;
-  const selectedLocationName = locationOptions.selectedOptions[0].value;
-  for (i = 0; i < locationFieldOptions.length; i++) {
-    if (selectedLocationName == locationFieldOptions[i].text) {
-      locationFieldOptions[i].selected = true;
+  // add the tagged place to the places field so that we can connect the article to these places on the timemap
+  const placeField = document.getElementById("id_place");
+  const placeFieldOptions = placeField.options;
+  const selectedPlaceName = placeOptions.selectedOptions[0].value;
+  for (i = 0; i < placeFieldOptions.length; i++) {
+    if (selectedPlaceName == placeFieldOptions[i].text) {
+      placeFieldOptions[i].selected = true;
     }
   }
 
-  var modal = document.getElementById("tagLocationModal");
+  var modal = document.getElementById("tagPlaceModal");
   modal.style.display = "none";
 }
