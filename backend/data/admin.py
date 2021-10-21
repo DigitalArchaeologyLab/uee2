584fca65c3b2c72ac031a038ba78b5ba80dd658a
Deidre Brin <dwhitmore@g.ucla.edu>
2021-10-21 16:11:27 -0700

    Add import/export options for images

--- backend/data/admin.py
+++ backend/data/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 from .models import Image
 
 
-class ImageAdmin(admin.ModelAdmin):
+class ImageAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ("title", "image_tag")
+
 
 admin.site.register(Image, ImageAdmin)
