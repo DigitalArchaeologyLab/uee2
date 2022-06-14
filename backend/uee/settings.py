6003169d22eaddd964d5b637b6926b82be7b54b3
Deidre Brin <deidre@brin.org>
2022-06-14 16:31:04 -0700

    Debug status based on env variable

--- backend/uee/settings.py
+++ backend/uee/settings.py
@@ -18,8 +18,7 @@ DATABASE_PORT = os.getenv("DATABASE_PORT")
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv("DEBUG_STATUS") == "True"
-DEBUG = True if str(os.getenv("DEBUG_STATUS", True)) == "True" else False
+DEBUG = True if os.getenv('DEBUG_STATUS', "True") == "True" else False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
