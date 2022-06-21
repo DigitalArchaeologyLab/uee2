123a1cf059bb1c3cd5079773ef6549accb2eee52
Deidre Brin <deidre@brin.org>
2022-06-21 12:54:22 -0700

    Fix typo in help text

--- backend/article/models.py
+++ backend/article/models.py
@@ -71,9 +71,21 @@ class Reference(models.Model):
     id = models.AutoField(primary_key=True)
     author = models.CharField("Author(s)", max_length=255)
     year = models.CharField("Year", max_length=20)
-    publication_info = RichTextField("Publication information", help_text="Enter the rest of the reference citation, following the bibliographic style used by the author (i.e. Form, fabric and function: Some observations on the cooking pot in antiquity, In Technology and style 2, Ceramics and Civilization. ed. W. David Kingery , pp. 157 - 172. Columbus: American Ceramic Society.")
-    url = models.URLField("Persistent URL", null=True, blank=True, help_text="If a DOI or other type of persistent identifier is available, add the DOI label here (i.e. doi:0000000/000000000000")
-    article = models.ManyToManyField("Article", blank=True, help_text="Select the article(s) this resource is referenced in - if an article is already selected, be sure to hold Control or Command when selecting an additional article.")
+    publication_info = RichTextField(
+        "Publication information",
+        help_text="Enter the rest of the reference citation, following the bibliographic style used by the author (i.e. Form, fabric and function: Some observations on the cooking pot in antiquity, In Technology and style 2, Ceramics and Civilization. ed. W. David Kingery , pp. 157 - 172. Columbus: American Ceramic Society.",
+    )
+    url = models.URLField(
+        "Persistent URL",
+        null=True,
+        blank=True,
+        help_text="If a DOI or other type of persistent identifier is available, add the DOI label here (i.e. doi:0000000/000000000000)",
+    )
+    article = models.ManyToManyField(
+        "Article",
+        blank=True,
+        help_text="Select the article(s) this resource is referenced in - if an article is already selected, be sure to hold Control or Command when selecting an additional article.",
+    )
 
     def __str__(self):
         return "%s (%s)" % (self.author, self.year)
