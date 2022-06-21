26895cd57b9f583e77a1419b9377c25ec5479673
Deidre Brin <deidre@brin.org>
2022-06-21 13:44:04 -0700

    Add and adjust fields associated with image resources

--- backend/data/models.py
+++ backend/data/models.py
@@ -4,6 +4,7 @@ from article.models import Article
 from timemap.models import Place
 from timemap.models import Period
 from collection.models import Collection
+from ckeditor.fields import RichTextField
 
 
 # Create your models here.
@@ -17,18 +18,37 @@ class Image(models.Model):
     description = models.TextField(
         "Full Description", max_length=1000, null=True, blank=True
     )
-    caption = models.TextField(
-        "Caption and credit line", max_length=1000, null=True, blank=True
-    )
+    caption = RichTextField("Caption", null=True, blank=True)
+    credit = RichTextField("Credit line", null=True, blank=True)
     creator = models.TextField(
         "Creator (photographer, illustrator, etc)",
         max_length=255,
         null=True,
         blank=True,
     )
-    # articles = models.ManyToManyField(Article)
+    dateCreated = models.CharField(
+        "Date created",
+        max_length=255,
+        null=True,
+        blank=True,
+        help_text="Date the original resource was created formatted as YYYY MM/DD. If month or day are not known, just enter the year (i.e. '2022 06' or '2022')",
+    )
     places = models.ManyToManyField(Place, blank=True)
     periods = models.ManyToManyField(Period, blank=True)
+    startDate = models.CharField(
+        "Start date",
+        max_length=255,
+        null=True,
+        blank=True,
+        help_text="Include a start date if a specific year is known; otherwise, just select the approproate period. Format the value as YYYY MM/DD. If month or day are not known, just enter the year (i.e. '2022 06' or '2022')",
+    )
+    endDate = models.CharField(
+        "End date",
+        max_length=255,
+        null=True,
+        blank=True,
+        help_text="Include an end date if a specific year is known; otherwise, just select the appropriate period. Format the value as YYYY MM/DD. If month or day are not known, just enter the year (i.e. '2022 06' or '2022')",
+    )
     persons = models.TextField(
         "Person(s) within content", max_length=1000, null=True, blank=True
     )
@@ -43,7 +63,13 @@ class Image(models.Model):
         "Permissions notes", max_length=1000, null=True, blank=True
     )
     collection = models.ManyToManyField(Collection, blank=True)
-    arkID = models.CharField("ArkID", max_length=255, null=True, blank=True, help_text="Add the Ark ID for this resource from the UCLA Library digital asset mananagement system (i.e. ark:/21198/zz000rwn8d)")
+    arkID = models.CharField(
+        "ArkID",
+        max_length=255,
+        null=True,
+        blank=True,
+        help_text="Add the Ark ID for this resource from the UCLA Library digital asset mananagement system (i.e. ark:/21198/zz000rwn8d). NOTE: This field is not fully implemented yet and entries will not be processed at this time.",
+    )
 
     def image_tag(self):
         if self.image_file:
