0adb88a728dfac69cc63ff28235086b634633f97
Deidre Brin <deidre@brin.org>
2022-08-05 09:37:45 -0700

    Add filtering to glossary admin page
    - Facilitate identifying and correcting empty definitions

--- backend/glossary/admin.py
+++ backend/glossary/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
+from django.contrib.admin.filters import EmptyFieldListFilter
+
 
 # Register your models here.
 from .models import GlossaryTerm
 
-
 class GlossaryAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ["term_eng", "term_ar", "definition_eng"]
+    list_filter = (
+        ("definition_eng", EmptyFieldListFilter),
+    )
 
 
 admin.site.register(GlossaryTerm, GlossaryAdmin)
