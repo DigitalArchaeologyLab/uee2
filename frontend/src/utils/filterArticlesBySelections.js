dd18b0265f1e17b54b51032d842b5967b3375732
Deidre Brin <deidrebrin@DAL-M2.local>
2023-04-11 16:14:02 -0700

    Add filtering for selected activity types

--- frontend/src/utils/filterArticlesBySelections.js
+++ frontend/src/utils/filterArticlesBySelections.js
@@ -4,5 +4,18 @@ export function filterArticlesBySelections(Articles, Activities, SelectedActivit
 
     const processedArticles = processData(Articles, Periods, Activities);
 
+    // filter processed article list by selected activity type(s)
+    let articlesWithSelectedTypes = [];
+    processedArticles.forEach((article) => {
+        SelectedActivityTypes.forEach((selectedType) => {
+            if (article.types.includes(selectedType) && !articlesWithSelectedTypes.includes(article)) {
+                articlesWithSelectedTypes.push(article)
+            }
+        })
+    })
+    // filter processed article list by selected min/max times
+
+    console.log(articlesWithSelectedTypes)
+
     return Articles;
 }
